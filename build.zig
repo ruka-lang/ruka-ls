ad9a8ed369a09a94e4ce0e212c7d2b9956c59630
dwclake <clake.dw@gmail.com>
2024-06-07 13:29:19 -0700

    update to zig 0.13.0

--- build.zig
+++ build.zig
@@ -44,7 +44,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
-        .test_runner = std.Build.LazyPath{.path = "test_runner.zig"},
+        .test_runner = b.path("test_runner.zig"),
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
@@ -54,7 +54,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-        .test_runner = std.Build.LazyPath{.path = "test_runner.zig"},
+        .test_runner = b.path("test_runner.zig"),
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
